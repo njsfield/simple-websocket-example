d1d68d61a254bd54ce19376a6b4a59d4c846b699
Nick Field <heyimnick@live.com>
2017-02-17 19:19:48 +0000

    update random string function to memoize

--- src/helpers/randomstring.js
+++ src/helpers/randomstring.js
@@ -1,6 +1,5 @@
-module.exports = (length = 8) => {
-  const chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  var result = '';
-  for (var i = length; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)];
-  return result;
+module.exports = () => {
+  return Math.random().toString(36).split('').filter((value, index, self) => {
+    return self.indexOf(value) === index;
+  }).join('').substr(2, 8);
 };
