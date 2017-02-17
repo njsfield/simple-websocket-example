07a7118e25d9d84557e67efdc0830fe3dbdeec4f
Nick Field <heyimnick@live.com>
2017-02-17 08:38:18 +0000

    refactor

--- websocket.js
+++ websocket.js
@@ -3,6 +3,7 @@ const server = require('./index');
 const io = require('socket.io')(server);
 const log = (msg) => process.stdout.write(`${msg}\n`);
 
+//
 io.on('connection', (ws) => {
   log('A new socket has been connected');
   ws.on('private message', (message) => {
